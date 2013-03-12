2d77399c347469f99ede2cb3640f2160e5bfb9df
Oliver Martell <oliver.martell@gmail.com>
2013-03-12 22:26:29 +0000

    Trying to improve specs

--- spec/LifeSpec.js
+++ spec/LifeSpec.js
@@ -5,7 +5,7 @@ var Life = (function() {
   }
 
   Life.prototype.nextGeneration = function() {
-    
+    return this.cells;
   };
 
   return Life;
@@ -21,31 +21,39 @@ var Cell = (function(){
 });
 
 describe("Conway's game of life", function() {
+  var _ = 0;
+  var empty = [
+    [_,_,_,_],
+    [_,_,_,_],
+    [_,_,_,_],
+    [_,_,_,_],
+    [_,_,_,_]
+  ];
 
   it("Given an empty seed, then there's no next generation", function() {
-    var empty = [
-      [ , , , ],
-      [ , , , ],
-      [ , , , ],
-      [ , , , ],
-      [ , , , ]
+    var seed = [
+      [_,_,_,_],
+      [_,_,_,_],
+      [_,_,_,_],
+      [_,_,_,_],
+      [_,_,_,_]
     ];
 
-    var life = new Life(empty);
-    expect(life.nextGeneration()).toBeEmpty();
+    var life = new Life(seed);
+    expect(life.nextGeneration()).toEqual(empty);
   });
 
   it("A cell with no neighbors dies on the next generation", function() {
     var seed = [
-      [ , , , ],
-      [ , , , ],
-      [ ,1, , ],
-      [ , , , ],
-      [ , , ,1]
+      [_,_,_,_],
+      [_,_,_,_],
+      [_,1,_,_],
+      [_,_,_,_],
+      [_,_,_,_]
     ];
 
     var life = new Life(seed);
-    expect(life.nextGeneration()).toBeEmpty();
+    expect(life.nextGeneration()).toEqual(empty);
   });
 
   xit("Given the following seed", function() {
@@ -53,16 +61,16 @@ describe("Conway's game of life", function() {
       [ , ,1, ],
       [ ,1, , ],
       [ ,1, , ],
-      [ , , , ],
-      [ , , , ]
+      [_,_,_,_],
+      [_,_,_,_]
     ];
 
     var nextGeneration = [
-      [ , , , ],
-      [ ,1,1, ],
-      [ , , , ],
-      [ , , , ],
-      [ , , , ]
+      [_,_,_,_],
+      [ ,1,1,_],
+      [_,_,_,_],
+      [_,_,_,_],
+      [_,_,_,_]
     ];
 
     var life = new Life(seed);
@@ -72,19 +80,19 @@ describe("Conway's game of life", function() {
 
   xit("Given the following seed", function() {
     var seed = [
-      [ , ,1, ],
-      [ ,1, , ],
-      [ ,1, , ],
-      [ , , , ],
-      [ , , , ]
+      [_,_,1,_],
+      [_,1,_,_],
+      [_,1,_,_],
+      [_,_,_,_],
+      [_,_,_,_]
     ];
 
     var nextGeneration = [
-      [ , , , ],
-      [ ,1,1, ],
-      [ , , , ],
-      [ , , , ],
-      [ , , , ]
+      [_,_,_,_],
+      [_,1,1,_],
+      [_,_,_,_],
+      [_,_,_,_],
+      [_,_,_,_]
     ];
 
     var life = new Life(seed);
--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,18 +1,7 @@
 beforeEach(function() {
   this.addMatchers({
-    toBePlaying: function(expectedSong) {
-      var player = this.actual;
-      return player.currentlyPlayingSong === expectedSong && 
-             player.isPlaying;
-    },
     toBeEmpty:  function(generation){
-      var empty = [
-        [ , , , ],
-        [ , , , ],
-        [ , , , ],
-        [ , , , ],
-        [ , , , ]
-      ];
+
       return generation === empty;
     }
   });
